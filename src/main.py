d4b2ae83ee340a723c0170b91546087061aa532a
Marcus Rigonati <mrigonati@purestorage.com>
2025-04-17 14:11:31 +0200

    Centralizing timer and button

--- src/main.py
+++ src/main.py
@@ -12,7 +12,7 @@ is_formatting = False # Prevents infinite loops during auto-formatting via trace
 
 # Create the main window
 window = tkinter.Tk()
-window.geometry("500x500")
+window.geometry("400x200")
 window.resizable(True, True)
 window.minsize(400, 200)
 window.maxsize(1000, 1000)
@@ -21,7 +21,7 @@ window.grid_columnconfigure(0, weight=1)
 window.title("PomoLocker")
 window.config(background="#ad504d")
 
-parent_frame = tkinter.Frame(window, padx=1, pady=1, background="#b5625f")
+parent_frame = tkinter.Frame(window, pady=10, background="#b5625f")
 parent_frame.grid(row=0, column=0)
 
 timer_var = tkinter.StringVar()
@@ -37,7 +37,7 @@ timer_entry = tkinter.Entry(
     borderwidth=0,
     highlightthickness=0,
 )
-timer_entry.pack(pady=20, padx=10, fill='x')
+timer_entry.grid(row=0, column=0, pady=10, padx=10)
 
 timer_entry_manager = TimerEntryManager(timer_var, timer_entry)
 
@@ -65,7 +65,7 @@ button_text = tkinter.StringVar(None, "Start")
 timer = Timer(window, parent_frame, timer_var, timer_entry, timer_entry_manager, button_text, style)
 
 button = ttk.Button(parent_frame, textvariable=button_text, command=timer.start_stop, style='Red.TButton')
-button.pack(pady=20)
+button.grid(row=1, column=0, pady=10)
 
 # Add callback in the correct order to format the input whenever the user types
 timer_entry.bindtags(((str(timer_entry)), "Entry", "post-processing", ".", "all"))
